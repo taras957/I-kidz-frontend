a9d8424ba70f148a34f18f640b9ce9b33ad8a62b
Taras Kornyk <50055416+taras957@users.noreply.github.com>
2021-07-22 00:26:33 +0300

    Update api-client.js

--- utils/api-client.js
+++ utils/api-client.js
@@ -1,6 +1,7 @@
 const apiURL = process.env.NEXT_PUBLIC_API;
 const apiPrefix = process.env.NEXT_PUBLIC_API_PREFIX;
 import axios from "axios";
+axios.defaults.withCredentials = true
 
 async function client(
   endpoint,
